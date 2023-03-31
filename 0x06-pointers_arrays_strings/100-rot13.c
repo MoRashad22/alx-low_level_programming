d1c1b944b6fca9670b2f2c9758758a48f06acf1d
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-03-30 19:58:08 -0700

    finished

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,7 +13,7 @@ char *rot13(char *s)
 	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	char *ptr = s;
-	
+
 	while (*s)
 	{
 		for (i = 0; i <= 52; i++)
