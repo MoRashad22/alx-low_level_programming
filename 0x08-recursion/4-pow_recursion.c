06c1497dae8cd41ade35356cb986ad793b1898e5
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-04-05 20:00:03 -0700

    liv task

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - finds the value of a given number
+ * rasied to the power of variable
+ * @x: given number
+ * @y: variable exposant
+ * Return: pointer ot matching byte in `s`
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if ( y == 1)
+		return (x);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
