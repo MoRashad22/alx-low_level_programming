e6f28e60be8b2b5c9467e6ab9fe5ea6ac092ff34
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-04-06 19:59:52 -0700

    Leo needs to go deeper

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,30 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: input to calculate its square root
- *
- * Return: the square root reault
+ * _sqrt_recursion - detect natural square root
+ * @n: int
+ * @rot: square root
+ * Return: int
  */
+int square(int n, int tor);
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	return (sqrt(n, 0));
+	return (found(n, 1));
 }
 
 /**
- * iamroot - recurses that find natural
- * square root of a number
- * @leo: number to calculate its sqaure root
- * @eve: int
- *
- * Return: the result
+ * found - find square root
+ * @n: int to find the square root
+ * rot: square root
+ * Return: int
  */
-int iamroot(int leo, int eve)
+
+int found(int n, int rot)
 {
-	if (leo * leo > eve)
+	if (rot * rot == n)
+		return (rot);
+	else if (rot * rot < n)
+		return (found(n, rot + 1));
+	else
 		return (-1);
-	if (leo * leo == eve)
-		return (leo);
-	return (iamroot(leo, eve + 1));
 }
