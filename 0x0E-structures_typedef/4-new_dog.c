2c84c431c87fe7b41a404408798272c57bfbd26e
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-04-18 22:12:55 -0700

    Dodger

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,6 +23,8 @@ int _strlen(const char *str)
  *
  * @src: string to copy
  * @dest: destination
+ *
+ * Return: @dest
  */
 
 char *_strcopy(char *dest, char *src)
@@ -52,6 +54,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (!name || age < 0 || !owner)
 		return (NULL);
+
 	dodger = (dog_t *)malloc(sizeof(dog_t));
 	if (dodger == NULL)
 		return (NULL);
@@ -70,9 +73,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dodger);
 		return(NULL);
 	}
-	_strcopy(dodger->name, name);
+	dodger->name = _strcopy(dodger->name, name);
 	dodger->age = age;
-	_strcopy(dodger->owner, owner);
+	dodger->owner = _strcopy(dodger->owner, owner);
 
 	return (dodger);
 }
