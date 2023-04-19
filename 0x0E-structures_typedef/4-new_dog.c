86f9d48bca5786e11aa495118c1dc242da650fc1
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-04-18 21:53:21 -0700

    Dodger

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,24 +2,40 @@
 #include "dog.h"
 
 /**
- * _strlen - gets string's length
- *
- * @str: string to be looked at
- * Return: @str length
+ * new_dog - does create new dog info
+ * @name: the dog's name
+ * @age: dog's age
+ * @owner: dog's owner
  */
 
-int _strlen(const char *str)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	int len = 0;
+	dot_t *dodger;
 
-	while (*str++)
-		len++;
-	return (len);
+	if (!name || age < 0 || !owner)
+		return (NULL);
+	dodger = (dog_t *)malloc(sizeof(dog_t));
+	if (dodger == NULL)
+		return (NULL);
+
+	dodger->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if ((*dodger).name == NULL)
+	{
+		free(dodger);
+		return (NULL);
+	}
+
+	dodger->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if ((*dodger).owner == NULL)
+	{
+		free(dodger->name);
+		free(dodger);
+		return(NULL);
+	}
+	_strcpy(dodger->name, name);
+	dodger->age = age;
+	_strcpy(dodger->owner, owner);
+
+	return (dodger);
 }
 
-/**
- * _strcopy - take a copy from @src to @dest
- *
- * @src: string to copy
- * @dest: destination
- * */
