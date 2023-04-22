65db1a158087cc39ad699dacdc6476314eec7f66
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-04-22 00:39:51 -0700

    finished

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,83 @@
+#include "cariadic_functions"
+
+/**
+ * print_char - prints character
+ * @sep: string separator
+ * @ap: argument pointer
+ */
+void print_char(char *sep, va_list ap)
+{
+	printf("%s%c", sep, va_arg(ap, int));
+}
+
+/**
+ * print_int - prints integer
+ * @sep: string separator
+ * @ap: argument pointer
+ */
+void print_int(char *sep, va_list ap)
+{
+	printf("%s%d", sep, va_arg(ap, int));
+}
+
+/**
+ * print_float - prints float
+ * @sep: string separator
+ * @ap: argument pointer
+ */
+void print_float(char *sep, va_list ap)
+{
+	printf("%s%f", sep, va_arg(ap, double));
+}
+
+/**
+ * print_string - printing string
+ * @sep: string separator
+ * @ap: argument pointer
+ */
+void print_string(char *sep, va_list ap)
+{
+	char *str = va_arg(ap, char *);
+
+	switch ((int)(!str))
+		case 1:
+			str = "(nil)";
+
+	printf("%s%s", sep, str);
+}
+
+/**
+ * print_all - does print anything
+ * @format: list of type of arguments passed to function
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0, j;
+	char *sep = "";
+	va_list ap;
+	print_t prints[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (prints[i].print)
+		{
+			if (format[i] == prints[j].print[0])
+			{
+				prints[j].f(sep, ap);
+				sep = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct print - new struct type
+ * @print: represent data type
+ * @f: function associated
+ */
+typedef struct print
+{
+	char *print;
+	void (*f)(char *, va_list);
+} print_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
