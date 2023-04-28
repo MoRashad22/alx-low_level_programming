314687db94ec7a3b445f74d44d3c297b0d3e6f5b
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-04-27 19:53:43 -0700

    finished

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,17 +9,16 @@
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
- * @len: the string length
+ * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * made late
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 int _putchar(char c);
