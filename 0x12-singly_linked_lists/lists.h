95141592df8063f907eae1ada7d2a99cedcdea77
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-04-28 07:26:48 -0700

    finished

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib>
-#include <string.h>
-#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -13,6 +13,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * made at mornin
  */
 typedef struct list_s
 {
@@ -21,8 +22,6 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
-int _putchar(char c);
-int _strlen(char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
