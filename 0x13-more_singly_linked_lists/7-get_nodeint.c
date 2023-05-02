a16e5b2321e3c02f58411262c92b3cfb821a0f47
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-05-02 16:12:17 -0700

    finishing

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *arrow;
 	unsigned int r;
 
-	for (r = 0; arrow && r < index; arrow = head, arrow = arrow->next, r++)
+	for (arrow = head, r = 0; arrow && r < index; arrow = arrow->next, r++)
 		;
 	return (arrow);
 }
