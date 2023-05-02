6b459c4addc347ad4f4c4c047bd438efac71cdde
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-05-02 14:37:13 -0700

    finishing

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * free_listint2 - freeing listint_t list
+ * @head: head pointer's pointer
+ * Return: nothing
+ * made at night
+ */
+void free_listint2(listint_t **head)
+{
+
