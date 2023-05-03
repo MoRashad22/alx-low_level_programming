7bacdd9030fe166972c19d33809fb528859a1eb2
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-05-02 18:06:52 -0700

    finishing

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * reverse_listint - reversing listint_t list
+ * @head: pointing to the head pointer
+ *
+ * Return: head's adress
+ * made at night
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint *revs = NULL, *nxt = NULL;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	while (*head)
+	{
+		nxt = (*head)->next;
+		(*head)->next = revs;
+		revs = *head;
+		*head = nxt;
+	}
+	return (*head);
+}
