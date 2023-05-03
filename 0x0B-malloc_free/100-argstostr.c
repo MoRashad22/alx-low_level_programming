2ad4d92a46a5b96aca1d5a3515671be2efdcabea
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-05-03 05:26:47 -0700

    finishing

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,7 +14,7 @@ char *argstostr(int ac, char **av)
 	int w, length, con, y;
 	char *updt;
 
-	if (ac == NULL || av == NULL)
+	if (ac == 0 || av == 0)
 	{
 		return (NULL);
 	}
@@ -28,13 +28,13 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	}
 	for (w = 0; w < ac; w++)
-	{
+
 		for (y = 0; av[w][y] != '\0'; y++)
-		{
+
 			updt[con] = av[w][y];
 			_putchar('\n');
-		}
+
 		updt[con] = '\0';
 		return (updt);
-	}
+
 }
