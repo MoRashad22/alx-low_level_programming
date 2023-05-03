c6b79499b831f76ee2b33a91ddfd0ac745c9b041
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-05-03 04:21:29 -0700

    finishing

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,8 +1,11 @@
-global		main
-extern		printf
+section	.text
+	global		main
+	extern		printf
+
 main:
-	mov		edi , format
+	mov		edi, format
 	mov		eax, 0
 	call	printf
 
-format db 'Hello, Holberton', 0xa, 0
+section	.data
+format: db 'Hello, Holberton', 0xa, 0
