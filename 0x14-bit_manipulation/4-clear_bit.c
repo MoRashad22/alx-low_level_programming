26878b4ebe449ef75f04506f652c2561864a201c
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-07-06 19:33:37 -0700

    finishing

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - clearing bit at index
+ * @n: number to index
+ * @index: bit to clear
+ *
+ * Return 1 when successful otherwise -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	if (*n & 1L << index)
+		*n ^= 1L << index;
+	return (1);
+}
