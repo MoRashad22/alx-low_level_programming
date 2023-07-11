132ea93c64b56a2370477dde05478c3d9e49a2be
MoRashad22 <mohamed.rashad6123@gmail.com>
2023-07-10 19:22:46 -0700

    finishing

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,6 +32,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+	fd = open(filename, O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 	if (len)
 		bytes = write(fd, text_content, len);
 	close(fd);
